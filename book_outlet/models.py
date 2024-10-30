1393e58a2af18f46a00445b90b500f461f1eaf9e
Srinivasan <srisan619@gmail.com>
2024-10-30 08:27:12 +0100

    slug field and slug update

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class Book(models.Model):
@@ -8,10 +9,16 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    # Harry Potter 1 => harry-potter-1
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self):
-        return f"{self.title} ({self.rating} {self.is_bestselling})"
+        return f"{self.id} {self.title} ({self.rating}) {self.is_bestselling} {self.author} {self.slug}"
         
     def get_absolute_url(self):
-        return reverse("book-detail", kwargs={"id": self.id})
+        return reverse("book-detail", kwargs={"slug": self.slug})
+    
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
     
--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index),
-    path("<int:id>", views.book_detail, name="book-detail")
+    path("", views.index, name="book-index"),
+    path("<slug:slug>", views.book_detail, name="book-detail")
 ]
